51a576d517a986dd9e98d85d59ef41be429e69e5
fllamas <chewfasa5@hotmail.com>
2023-11-22 20:48:03 -0800

    Get movie information from a search query by using a string for the movie title.

--- test_fetch_movie_poster.py
+++ test_fetch_movie_poster.py
@@ -16,7 +16,15 @@ main_frame.rowconfigure(0, weight=1)
 
 #raw_data = urllib.request.urlopen('https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg').read()
 
-movie_id = 343611 
+#movie_id = 343611
+search_movie = "Jack Reacher: Never Go Back"
+get_movie_info = requests.get('https://api.themoviedb.org/3/search/movie?query={}&api_key=0445e86644dc5e6dde39ce605f795cd5&language=en-US'.format(search_movie))
+movie_info = get_movie_info.json()
+# A list of dictionary, results is a list that holds a dictionary. 
+# So get data in first index (0) of key 'id'
+movie_id = movie_info['results'][0]['id']
+#print(movie_id)
+
 response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=0445e86644dc5e6dde39ce605f795cd5&language=en-US'.format(movie_id))
 data = response.json()
 movie_poster = "https://image.tmdb.org/t/p/w500/" + data['poster_path']
