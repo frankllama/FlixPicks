2c9305c35a17d3dad3aa5d7469dabacefcbc1802
fllamas <chewfasa5@hotmail.com>
2023-11-22 20:51:56 -0800

    Added some comments to what code is searching for the movie info and what code is retrieving the poster using it's id with tmdb server.

--- test_fetch_movie_poster.py
+++ test_fetch_movie_poster.py
@@ -16,6 +16,7 @@ main_frame.rowconfigure(0, weight=1)
 
 #raw_data = urllib.request.urlopen('https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg').read()
 
+# Get movie id from searching a movie
 #movie_id = 343611
 search_movie = "Jack Reacher: Never Go Back"
 get_movie_info = requests.get('https://api.themoviedb.org/3/search/movie?query={}&api_key=0445e86644dc5e6dde39ce605f795cd5&language=en-US'.format(search_movie))
@@ -25,6 +26,7 @@ movie_info = get_movie_info.json()
 movie_id = movie_info['results'][0]['id']
 #print(movie_id)
 
+# Get movie poster path from it's id with the tmdb api.
 response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=0445e86644dc5e6dde39ce605f795cd5&language=en-US'.format(movie_id))
 data = response.json()
 movie_poster = "https://image.tmdb.org/t/p/w500/" + data['poster_path']
