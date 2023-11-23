706cad80618eb166c0981762988f0feffbf63ced
fllamas <chewfasa5@hotmail.com>
2023-11-22 21:46:28 -0800

    Moved code to get movie posters into a function/ module. Need to create separate lables and image objects for each movie.

--- test_fetch_movie_poster.py
+++ test_fetch_movie_poster.py
@@ -7,7 +7,7 @@ import requests
 
 root = tk.Tk()
 root.geometry('800x800')
-root.resizable(width=False, height=False)
+root.resizable(width=True, height=True)
 
 main_frame = tk.Frame(root, bg='WHITE')
 main_frame.pack(fill=tk.BOTH, expand=True)
@@ -16,31 +16,50 @@ main_frame.rowconfigure(0, weight=1)
 
 #raw_data = urllib.request.urlopen('https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg').read()
 
-# Get movie id from searching a movie
-#movie_id = 343611
+def get_movie_poster(search_movie):
+    # Get movie id from searching a movie
+    #movie_id = 343611
+    #search_movie = "Jack Reacher: Never Go Back"
+    get_movie_info = requests.get('https://api.themoviedb.org/3/search/movie?query={}&api_key=0445e86644dc5e6dde39ce605f795cd5&language=en-US'.format(search_movie))
+    movie_info = get_movie_info.json()
+    # A list of dictionary, results is a list that holds a dictionary. 
+    # So get data in first index (0) of key 'id'
+    movie_id = movie_info['results'][0]['id']
+    #print(movie_id)
+
+    # Get movie poster path from it's id with the tmdb api.
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=0445e86644dc5e6dde39ce605f795cd5&language=en-US'.format(movie_id))
+    data = response.json()
+    movie_poster = "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+    raw_data = urllib.request.urlopen(movie_poster).read()
+
+    image_obj = ImageTk.PhotoImage(Image.open(io.BytesIO(raw_data)))
+    return image_obj
+
+
 search_movie = "Jack Reacher: Never Go Back"
-get_movie_info = requests.get('https://api.themoviedb.org/3/search/movie?query={}&api_key=0445e86644dc5e6dde39ce605f795cd5&language=en-US'.format(search_movie))
-movie_info = get_movie_info.json()
-# A list of dictionary, results is a list that holds a dictionary. 
-# So get data in first index (0) of key 'id'
-movie_id = movie_info['results'][0]['id']
-#print(movie_id)
+image_obj_1 = get_movie_poster(search_movie)
 
-# Get movie poster path from it's id with the tmdb api.
-response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=0445e86644dc5e6dde39ce605f795cd5&language=en-US'.format(movie_id))
-data = response.json()
-movie_poster = "https://image.tmdb.org/t/p/w500/" + data['poster_path']
-
-raw_data = urllib.request.urlopen(movie_poster).read()
-
-image_obj = ImageTk.PhotoImage(Image.open(io.BytesIO(raw_data)))
-
-label_img = tk.Label(
+label_img_1 = tk.Label(
         main_frame,
-        image=image_obj,
-        bg='WHITE'
+        image=image_obj_1,
+        bg='WHITE',
+        padx=2,
         )
 
-label_img.grid(column=0, row=0)
+label_img_1.grid(column=0, row=0)
+
+search_movie = "Fast X"
+image_obj_2 = get_movie_poster(search_movie)
+
+label_img_2 = tk.Label(
+        main_frame,
+        image=image_obj_2,
+        bg='WHITE',
+        padx=2
+        )
+
+label_img_2.grid(column=1, row=0)
 
 root.mainloop()
